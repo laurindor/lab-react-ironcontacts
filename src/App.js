1f577aacd4ba4965a942ee0166dd18d7ebe9b9f1
laurindor <arual.socram@gmail.com>
2021-07-14 21:39:08 +0200

    done but it 2 meh

--- src/App.js
+++ src/App.js
@@ -1,37 +1,86 @@
-import React from 'react'
-import './App.css';
-import contactsData from "./contacts.json";
-import AddRandom from './components/AddRandom';
+import React from "react";
+import "./App.css";
+import contacts from "./contacts.json";
 
 
-function App() {
-  return ( 
-    <div className='App'>
-      <AddRandom />
-       <div className='ContactsList'>
-       <h1>Ironcontacts</h1>
-      {contactsData.filter((contact, idx)=>idx<5).map((contact)=>{
-        return(
-          <div>
-          <table>
-         <tr>
-            <th>Picture</th>
-             <th>Name</th>
-            <th>Popularity</th>
-          </tr>
+class App extends React.Component {
 
-         <tr>
-            <td> <img className="imgcontact" src={contact.pictureUrl} alt="imgcontact"></img> </td> 
-            <td>{contact.name}</td>
-            <td>{contact.popularity}</td>      
-         </tr>
-         </table>
-      
-        </div>
+  state = {
+    contacts: contacts.slice(0, 5),
+  }
 
-      )})} </div>
-    </div>   
-      )}
+  addRandom = () => {
+    let randomContact = contacts[Math.floor(Math.random() * contacts.length)] 
+    this.setState(() => {
+     return {contacts : [...this.state.contacts, randomContact]}
+    })
+  }
 
- 
+
+  sortByName = () => {
+    let sortName = [...this.state.contacts].sort((a, b) => a.name.localeCompare(b.name))
+    this.setState(() => {
+      return {contacts: sortName}
+    })
+  }
+
+
+  sortByPopularity = () => {
+    let mostPopular = [...this.state.contacts].sort((a, b) => b.popularity - a.popularity)
+    this.setState(() => {
+      return {contacts: mostPopular}
+    })
+  }
+
+
+  removeContact = (index) => {
+    const deleteContact = [...this.state.contacts];
+    deleteContact.splice(index, 1);
+    this.setState({
+      contacts: deleteContact
+    });
+  }
+
+
+
+  render() {
+    console.log("Render App.js")
+    console.log(this.state.contacts)
+    return (
+      <div className="App">
+        <h1>Ironcontacts</h1>
+        <button onClick={() => this.addRandom()}>Add Random Actor</button>
+        <button onClick={this.sortByName}>Sort Actors By Name</button>
+        <button onClick={() => this.sortByPopularity()}>Sort Actors By Popularity</button>
+        <table>
+          <thead>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+        </thead>
+        <tbody>
+      { this.state.contacts.map((contact) => {
+
+             return (
+              <tr key={contact.id}>
+              <td><img src={contact.pictureUrl}/></td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <button onClick={() => this.removeContact()}>Delete</button>
+              </tr>
+              )
+             }       
+          )}
+        </tbody>
+</table>
+</div>
+);
+}
+}
+
+
 export default App;
+
+
